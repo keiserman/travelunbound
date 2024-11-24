ab5b9da9385bea34e366d1e29f394bab2554bbaf
Kevin <eisermanka@gmail.com>
2024-11-24 17:38:28 -0500

    Add theme mods

--- template-parts/experiences.php
+++ template-parts/experiences.php
@@ -1,25 +1,25 @@
 <div class="grid grid-cols-12 gap-20">
-    <div class="col-start-2 col-span-10 grid gap-4">
-        <img src="/" alt="" class="w-full aspect-video bg-zinc-100">
+    <div data-animate="fade" class="col-start-2 col-span-10 grid gap-4">
+        <img src="<?php echo esc_url(get_theme_mod('experience_1_image', '')); ?>" alt="" class="w-full">
         <div class="flex gap-4 justify-between align-middle">
-            <h4>beach/getaways</h4>
-            <div>One & Only, Mandarina Mexico</div>
+            <h4><?php echo esc_html(get_theme_mod('experience_1_title', 'Default Experience 1 Title')); ?></h4>
+            <div><?php echo esc_html(get_theme_mod('experience_1_excerpt', 'Default Experience 1 Excerpt')); ?></div>
         </div>
     </div>
 
-    <div class="col-start-1 col-span-6 grid gap-4">
-        <img src="/" alt="" class="w-full aspect-[2/3] bg-zinc-100">
+    <div data-animate="fade" class="col-start-1 col-span-6 grid gap-4">
+        <img src="<?php echo esc_url(get_theme_mod('experience_2_image', '')); ?>" alt="" class="w-full">
         <div class="flex gap-4 justify-between align-middle">
-            <h4>beach/getaways</h4>
-            <div>One & Only, Mandarina Mexico</div>
+            <h4><?php echo esc_html(get_theme_mod('experience_2_title', 'Default Experience 2 Title')); ?></h4>
+            <div><?php echo esc_html(get_theme_mod('experience_2_excerpt', 'Default Experience 2 Excerpt')); ?></div>
         </div>
     </div>
 
-    <div class="col-span-6 grid gap-4">
-        <img src="/" alt="" class="w-full aspect-[2/3] bg-zinc-100">
+    <div data-animate="fade" class="col-span-6 grid gap-4">
+        <img src="<?php echo esc_url(get_theme_mod('experience_3_image', '')); ?>" alt="" class="w-full">
         <div class="flex gap-4 justify-between align-middle">
-            <h4>beach/getaways</h4>
-            <div>One & Only, Mandarina Mexico</div>
+            <h4><?php echo esc_html(get_theme_mod('experience_3_title', 'Default Experience 3 Title')); ?></h4>
+            <div><?php echo esc_html(get_theme_mod('experience_3_excerpt', 'Default Experience 3 Excerpt')); ?></div>
         </div>
     </div>
 
