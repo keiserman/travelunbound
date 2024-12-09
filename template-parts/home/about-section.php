718024f7312ad500fc7d8336fd7b51c238e48f7d
Kevin <eisermanka@gmail.com>
2024-12-08 23:41:05 -0500

    Fix about video and image clip paths

--- template-parts/home/about-section.php
+++ template-parts/home/about-section.php
@@ -4,18 +4,20 @@
         <h2 class="heading-h2 m-auto max-w-3xl mt-44 mb-12">introducing travel/<span class="font-sans">unbound</span></h2>
 
         <div class="container mb-20">
-            <div class="relative">
-
+            <div class="grid grid-cols-2 aspect-video">
                 <?php
                 $about_video_id = get_theme_mod('about_video');
                 $about_video_url = $about_video_id ? wp_get_attachment_url($about_video_id) : '';
                 ?>
+                <div class="relative h-full clip-path-about-video -mr-[12%]">
+                    <video autoplay muted loop class="h-full w-full top-0 absolute object-cover">
+                        <source src="<?php echo esc_url($about_video_url); ?>" type="video/mp4">
+                    </video>
+                </div>
 
-                <video autoplay muted loop class="aspect-video w-full">
-                    <source src="<?php echo esc_url($about_video_url); ?>" type="video/mp4">
-                </video>
-
-                <img src="<?php echo esc_url(get_theme_mod('about_image')); ?>" alt="" class="absolute top-0 right-0 h-full w-2/3 object-cover clip-path-custom">
+                <div class="relative h-full -ml-[12%] clip-path-about-image">
+                    <img src="<?php echo esc_url(get_theme_mod('about_image')); ?>" alt="" class="h-full w-full object-cover absolute">
+                </div>
             </div>
             <div class="text-base text-right py-2">One & Only, Mandarina Mexico</div>
         </div>
