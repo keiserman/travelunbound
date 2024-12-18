128136b9eda6cff02ea8ca27181d7da1ab710296
Kevin <eisermanka@gmail.com>
2024-12-18 08:55:58 -0500

    Remove duplicate display class

--- template-parts/home/section_experiences.php
+++ template-parts/home/section_experiences.php
@@ -45,7 +45,7 @@
                 </div>
             </div>
 
-            <div id="<?php echo $modal_id; ?>" class="lightbox-modal fixed inset-0 z-50 bg-primary hidden flex items-center justify-center" data-gallery-modal>
+            <div id="<?php echo $modal_id; ?>" class="lightbox-modal fixed inset-0 z-50 bg-primary hidden items-center justify-center" data-gallery-modal>
                 <button id="closeLightboxBtn" class="close-lightbox absolute w-8 h-8 top-4 right-4 text-white text-2xl z-10">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 256 256">
                         <path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>
