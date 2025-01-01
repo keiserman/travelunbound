fd40addfbd311a4864f93ab0367a4e25eefbedb5
Kevin <eisermanka@gmail.com>
2024-12-31 19:25:09 -0500

    Add content styling to contact page

--- template-parts/contact/section_contact.php
+++ template-parts/contact/section_contact.php
@@ -9,11 +9,7 @@ $contact_contact_paragraph = esc_html(get_theme_mod("contact_contact_paragraph")
         <div class="container-small grid gap-12">
             <h2 class="heading-h2"><?php echo $contact_contact_heading_main ?> <span class="font-sans"><?php echo $contact_contact_heading_span ?></span></h2>
             <p class="text-xl"><?php echo $contact_contact_paragraph ?></p>
-            <ol class="pl-8 border-l-8 border-secondary list-decimal list-inside text-xl flex flex-col gap-2">
-                <li>fill out enquiry or message us below</li>
-                <li>we'll get back to you with xyz</li>
-            </ol>
-            <?php the_content(); ?>
+            <div class="contact-content"><?php the_content(); ?></div>
         </div>
     </div>
 </section>
