5b9099993a300da25e4e163c5d4f9316035e3d97
Kevin <eisermanka@gmail.com>
2025-01-05 22:46:20 -0500

    Limit FAQs on about page

--- page-base.php
+++ page-base.php
@@ -64,18 +64,18 @@ Template Name: Base Page
                         <li>Private Jet Charter</li>
                         <li>Cruise Planning</li>
                     </ul>
-                    <div class="faq-list">
+                    <div>
                         <?php
-                        $faq_query = new WP_Query(array(
-                            'post_type' => 'faq',
-                            'posts_per_page' => -1,
-                        ));
-                        if ($faq_query->have_posts()) :
-                            while ($faq_query->have_posts()) :
-                                $faq_query->the_post();
-                        ?>
+                        $faq_query = new WP_Query([
+                            "post_type" => "faq",
+                            "posts_per_page" => -1,
+                        ]);
+
+                        if ($faq_query->have_posts()):
+                            while ($faq_query->have_posts()):
+                                $faq_query->the_post(); ?>
                                 <div faq class="border-b border-primary overflow-hidden">
-                                    <div faq-trigger class="text-xl py-2 flex gap-4 justify-between items-center cursor-pointer">
+                                    <div faq-trigger class="text-xl py-4 flex gap-4 justify-between items-center cursor-pointer">
                                         <?php the_title(); ?>
                                         <div class="relative w-4 h-4 flex items-center">
                                             <div class="w-4 h-0.5 bg-primary"></div>
@@ -83,19 +83,26 @@ Template Name: Base Page
                                         </div>
                                     </div>
                                     <div faq-content class="text-xl h-0 opacity-0">
-                                        <p class="pb-4"><?php echo wp_strip_all_tags(get_the_content()); ?></p>
+                                        <div class="pb-8">
+                                            <?php echo apply_filters(
+                                                "the_content",
+                                                get_the_content()
+                                            ); ?>
+                                        </div>
                                     </div>
                                 </div>
                         <?php
                             endwhile;
                         endif;
+
                         wp_reset_postdata();
                         ?>
                     </div>
-                    <div class="flex">
-                        <a href="" class="btn-outline">button static</a>
+                    <div class="flex flex-wrap gap-4">
+                        <a href="" class="btn-primary">button static</a>
                         <a href="" class="btn-outline">button static</a>
                     </div>
+                    <?php the_content(); ?>
                 </div>
             </div>
         </div>
--- template-parts/about/section_mission.php
+++ template-parts/about/section_mission.php
@@ -24,7 +24,38 @@ $about_mission_button_link = esc_url(get_permalink(get_theme_mod("about_mission_
                         <p class="text-lg md:text-xl mb-6">it's only natural to have some questions before you book your trip. we've answered the most commonly asked ones below. this way, you can understand more about who we are, what we do and what we stand for through the entire travel experience.</p>
 
                         <div class="mb-10">
-                            <?php get_template_part("template-parts/faqs"); ?>
+                            <?php
+                            $faq_query = new WP_Query([
+                                "post_type" => "faq",
+                                "posts_per_page" => 3,
+                            ]);
+
+                            if ($faq_query->have_posts()):
+                                while ($faq_query->have_posts()):
+                                    $faq_query->the_post(); ?>
+                                    <div faq class="border-b border-white overflow-hidden">
+                                        <div faq-trigger class="text-xl py-4 flex gap-4 justify-between items-center cursor-pointer">
+                                            <?php the_title(); ?>
+                                            <div class="relative w-4 h-4 flex items-center">
+                                                <div class="w-4 h-0.5 bg-white"></div>
+                                                <div faq-icon-line class="absolute rotate-90 w-4 h-0.5 bg-white"></div>
+                                            </div>
+                                        </div>
+                                        <div faq-content class="text-xl h-0 opacity-0">
+                                            <div class="pb-8">
+                                                <?php echo apply_filters(
+                                                    "the_content",
+                                                    get_the_content()
+                                                ); ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                            <?php
+                                endwhile;
+                            endif;
+
+                            wp_reset_postdata();
+                            ?>
                         </div>
                         <div class="flex">
                             <?php if (
