e85eb200625d2587fbefbbf1398a3b2dd3c3be00
Kevin <eisermanka@gmail.com>
2025-01-06 14:49:41 -0500

    Update statement section element order on mobile

--- template-parts/about/section_mission.php
+++ template-parts/about/section_mission.php
@@ -9,72 +9,70 @@ $about_mission_faq_text = get_theme_mod("about_mission_faq_text");
 
 <section data-animate="scroll-bg">
     <div class="padding-global py-6">
-        <div class="grid gap-5">
-            <div class="text-lg mb-24 md:mb-60">our mission</div>
-            <h2 class="text-[2rem] lg:text-[4.35rem] font-serif leading-none">Travel can enlighten, inspire, and heal, serving as a transformative force in our lives. Yet, in today's fast-paced world, this ideal often feels out of reach.</h2>
-            <div class="grid lg:grid-cols-2 gap-6 items-stretch">
-                <div class="relative lg:order-none order-last">
-                    <img src="<?php echo $about_mission_image; ?>" alt="<?php echo $about_mission_photo_credit; ?>" class="aspect-[4/5] lg:h-full w-full object-cover">
-                    <div class="text-sm lg:vertical-text lg:absolute lg:bottom-0 lg:-right-6"><?php echo esc_html($about_mission_photo_credit); ?></div>
-                </div>
-                <div class="flex flex-col justify-between">
-                    <div class="w-[93%] flex flex-col justify-start gap-2 mb-8">
-                        <h2 class="-mt-5 text-[2rem] lg:text-[4.25rem] leading-none">Our mission is to help you rediscover the joys and meaning of travel, creating enriching experiences without the stress, uncertainty and friction that usually come with it.</h2>
-                    </div>
-                    <div class="flex gap-2 flex-col lg:pl-20 lg:pt-20">
-                        <?php if (!empty($about_mission_faq_title)): ?>
-                            <div class="font-semibold text-xl"><?php echo esc_html($about_mission_faq_title); ?></div>
-                        <?php endif; ?>
-                        <?php if (!empty($about_mission_faq_text)): ?>
-                            <p class="text-lg md:text-xl mb-6"><?php echo esc_html($about_mission_faq_text); ?></p>
-                        <?php endif; ?>
-                        <div class="mb-10">
-                            <?php
-                            $faq_query = new WP_Query([
-                                "post_type" => "faq",
-                                "posts_per_page" => 3,
-                            ]);
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-5">
+            <div class="lg:col-span-2 text-lg mb-24 md:mb-60">our mission</div>
+            <h2 class="lg:col-span-2 text-[2rem] lg:text-[4.35rem] font-serif leading-none">Travel can enlighten, inspire, and heal, serving as a transformative force in our lives. Yet, in today's fast-paced world, this ideal often feels out of reach.</h2>
 
-                            if ($faq_query->have_posts()):
-                                while ($faq_query->have_posts()):
-                                    $faq_query->the_post(); ?>
-                                    <div faq class="border-b border-white overflow-hidden">
-                                        <div faq-trigger class="text-xl py-4 flex gap-4 justify-between items-center cursor-pointer">
-                                            <?php the_title(); ?>
-                                            <div class="relative w-4 h-4 flex items-center">
-                                                <div class="w-4 h-0.5 bg-white"></div>
-                                                <div faq-icon-line class="absolute rotate-90 w-4 h-0.5 bg-white"></div>
-                                            </div>
-                                        </div>
-                                        <div faq-content class="text-xl h-0 opacity-0">
-                                            <div class="pb-8">
-                                                <?php echo apply_filters(
-                                                    "the_content",
-                                                    get_the_content()
-                                                ); ?>
-                                            </div>
-                                        </div>
+            <div class="lg:row-span-3 relative lg:order-none order-last">
+                <img src="<?php echo $about_mission_image; ?>" alt="<?php echo $about_mission_photo_credit; ?>" class="aspect-[4/5] lg:h-full w-full object-cover">
+                <div class="text-sm lg:vertical-text lg:absolute lg:bottom-0 lg:-right-6"><?php echo esc_html($about_mission_photo_credit); ?></div>
+            </div>
+            <div class="w-[93%] flex flex-col justify-start gap-2 lg:mb-8">
+                <h2 class="-mt-5 text-[2rem] lg:text-[4.25rem] leading-none">Our mission is to help you rediscover the joys and meaning of travel, creating enriching experiences without the stress, uncertainty and friction that usually come with it.</h2>
+            </div>
+            <div class="flex gap-2 flex-col lg:pl-20 lg:pt-20 order-last lg:order-none">
+                <?php if (!empty($about_mission_faq_title)): ?>
+                    <div class="font-semibold text-xl"><?php echo esc_html($about_mission_faq_title); ?></div>
+                <?php endif; ?>
+                <?php if (!empty($about_mission_faq_text)): ?>
+                    <p class="text-lg md:text-xl mb-6"><?php echo esc_html($about_mission_faq_text); ?></p>
+                <?php endif; ?>
+                <div class="mb-10">
+                    <?php
+                    $faq_query = new WP_Query([
+                        "post_type" => "faq",
+                        "posts_per_page" => 3,
+                    ]);
+
+                    if ($faq_query->have_posts()):
+                        while ($faq_query->have_posts()):
+                            $faq_query->the_post(); ?>
+                            <div faq class="border-b border-white overflow-hidden">
+                                <div faq-trigger class="text-xl py-4 flex gap-4 justify-between items-center cursor-pointer">
+                                    <?php the_title(); ?>
+                                    <div class="relative w-4 h-4 flex items-center">
+                                        <div class="w-4 h-0.5 bg-white"></div>
+                                        <div faq-icon-line class="absolute rotate-90 w-4 h-0.5 bg-white"></div>
                                     </div>
-                            <?php
-                                endwhile;
-                            endif;
+                                </div>
+                                <div faq-content class="text-xl h-0 opacity-0">
+                                    <div class="pb-8">
+                                        <?php echo apply_filters(
+                                            "the_content",
+                                            get_the_content()
+                                        ); ?>
+                                    </div>
+                                </div>
+                            </div>
+                    <?php
+                        endwhile;
+                    endif;
 
-                            wp_reset_postdata();
-                            ?>
-                        </div>
-                        <div class="flex">
-                            <?php if (
-                                !empty($about_mission_button_link) &&
-                                !empty($about_mission_button_text)
-                            ): ?>
-                                <a href="<?php echo $about_mission_button_link; ?>" class="btn-outline-white">
-                                    <?php echo esc_html($about_mission_button_text); ?>
-                                </a>
-                            <?php endif; ?>
-                        </div>
-                    </div>
+                    wp_reset_postdata();
+                    ?>
+                </div>
+                <div class="flex">
+                    <?php if (
+                        !empty($about_mission_button_link) &&
+                        !empty($about_mission_button_text)
+                    ): ?>
+                        <a href="<?php echo $about_mission_button_link; ?>" class="btn-outline-white">
+                            <?php echo esc_html($about_mission_button_text); ?>
+                        </a>
+                    <?php endif; ?>
                 </div>
             </div>
+
         </div>
     </div>
 </section>
--- template-parts/home/section_journey.php
+++ template-parts/home/section_journey.php
@@ -7,31 +7,28 @@ $home_journey_button_link = esc_url(get_permalink(get_theme_mod("home_journey_bu
 
 <section data-animate="scroll-bg">
     <div class="padding-global py-6">
-        <div class="grid gap-5">
-            <h2 class="text-3xl sm:text-[3.06rem] md:text-[4.4rem] font-serif leading-none">in today's connected world, the endless online chatter and constant state of flux can make planning your holiday feel overwhelming.</h2>
-            <div class="grid lg:grid-cols-2 gap-6 items-stretch">
-                <div class="relative lg:order-none order-first">
-                    <img src="<?php echo $home_journey_image ?>" alt="<?php echo $home_journey_photo_credit ?>" class="aspect-[4/5] lg:h-full w-full object-cover">
-                    <div class="text-sm lg:vertical-text lg:absolute lg:bottom-0 lg:-right-6"><?php echo $home_journey_photo_credit ?></div>
-                </div>
-                <div class="flex flex-col gap-4 justify-between">
-                    <div class="flex flex-col justify-start gap-4">
-                        <h2 class="-mt-5 text-3xl sm:text-[3.06rem] md:text-[4.25rem] leading-none">we navigate the fast-changing landscape, cut through the chaos and remove stress and uncertainty from the process, so you can fully immerse yourself in the joy and excitement of your travel experience.</h2>
-                        <?php if (
-                            !empty($home_journey_button_link) &&
-                            !empty($home_journey_button_text)
-                        ): ?>
-                            <a href="<?php echo $home_journey_button_link; ?>" class="btn-outline-white hover:text-secondary self-start">
-                                <?php echo esc_html($home_journey_button_text); ?>
-                            </a>
-                        <?php endif; ?>
-                    </div>
-                    <div class="flex gap-2 flex-col lg:pl-20 lg:pt-20">
-                        <p class="text-lg md:text-xl font-medium">Crafted for the modern traveller</p>
-                        <p class="text-lg md:text-xl">we understand the hectic nature of modern life where multitasking is almost second mature; constant demands on our time and attention, the relentless struggle for work/life balance, competing priorities and an ever-growing to-do list. we save you time and work to your pace.</p>
-                    </div>
-                </div>
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-5">
+            <h2 class="lg:col-span-2 text-3xl sm:text-[3.06rem] md:text-[4.4rem] font-serif leading-none">in today's connected world, the endless online chatter and constant state of flux can make planning your holiday feel overwhelming.</h2>
+            <div class="lg:row-span-3 relative lg:order-none order-last">
+                <img src="<?php echo $home_journey_image ?>" alt="<?php echo $home_journey_photo_credit ?>" class="aspect-[4/5] lg:h-full w-full object-cover">
+                <div class="text-sm lg:vertical-text lg:absolute lg:bottom-0 lg:-right-6"><?php echo $home_journey_photo_credit ?></div>
             </div>
+            <h2 class="-mt-5 text-3xl sm:text-[3.06rem] md:text-[4.25rem] leading-none">we navigate the fast-changing landscape, cut through the chaos and remove stress and uncertainty from the process, so you can fully immerse yourself in the joy and excitement of your travel experience.</h2>
+            <?php if (
+                !empty($home_journey_button_link) &&
+                !empty($home_journey_button_text)
+            ): ?>
+                <div class="flex">
+                    <a href="<?php echo $home_journey_button_link; ?>" class="btn-outline-white hover:text-secondary self-start">
+                        <?php echo esc_html($home_journey_button_text); ?>
+                    </a>
+                </div>
+            <?php endif; ?>
+            <div class="flex justify-end gap-2 flex-col lg:pl-20 lg:pt-20 order-last lg:order-none">
+                <p class="text-lg md:text-xl font-medium">Crafted for the modern traveller</p>
+                <p class="text-lg md:text-xl">we understand the hectic nature of modern life where multitasking is almost second mature; constant demands on our time and attention, the relentless struggle for work/life balance, competing priorities and an ever-growing to-do list. we save you time and work to your pace.</p>
+            </div>
+
         </div>
     </div>
 </section>
